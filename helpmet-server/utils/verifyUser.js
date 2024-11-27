f58f581a59e2b3213637ac47b1b179d4a94c5f4e
ccyeung <ccyeung22@gmail.com>
2024-11-26 21:18:42 -0800

    err

--- helpmet-server/utils/verifyUser.js
+++ helpmet-server/utils/verifyUser.js
@@ -3,12 +3,17 @@ const { errorHandler } = require('./error');
 
 exports.verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
+  console.log("Token received:", token);
+  
 
   if (!token) return next(errorHandler(401, 'Unauthorized'));
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return next(errorHandler(403, 'Forbidden'));
-
+    console.log("Decoded User:", user);
+    if (err) {
+      console.error("JWT Verification Error:", err.message);
+      return next(errorHandler(403, 'Forbidden'));
+    }
     req.user = user;
     next();
   });
